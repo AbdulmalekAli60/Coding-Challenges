603816449b037ea060681eb0e2b6a9971a28ac53
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-09-06 07:51:09 +0300

    nothing

--- Coding Challanges/RemoveDuplicatesfromSortedList.java
+++ Coding Challanges/RemoveDuplicatesfromSortedList.java
@@ -1,22 +1,22 @@
 import MergeTwoSortedLists.MergeSortedList.ListNode;
 
 public class RemoveDuplicatesfromSortedList {
-    public ListNode deleteDuplicates(ListNode head) {
-        return delete(head);
-    }
+    // public ListNode deleteDuplicates(ListNode head) {
+        // return delete(head);
+    // }
 
-    public ListNode delete(ListNode head){
+    // public ListNode delete(ListNode head){
 
-        if(head == null || head.next == null){
-            return head;
-        } 
+    //     if(head == null || head.next == null){
+    //         return head;
+    //     } 
 
-        while(head.next != null && head.val == head.next.val){
-            head.next = head.next.next; 
-        }
+    //     while(head.next != null && head.val == head.next.val){
+    //         head.next = head.next.next; 
+    //     }
 
-        head.next = delete(head.next); 
+    //     head.next = delete(head.next); 
 
-        return head;
-    }
+    //     return head;
+    // }
 }
