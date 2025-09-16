7242867ab0d4992acdf04bcbf2eb7b43150f1e6d
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-09-16 15:39:02 +0300

    Remove element from list node

---
+++ Coding Challanges/RemoveElementsFromListNode.java
@@ -0,0 +1,18 @@
+
+// public class RemoveElementsFromListNode {
+//     public ListNode removeElements(ListNode head, int val) {
+        
+//         if(head == null) return head;
+
+//         if(head.val != val){ 
+//            head.next = removeElements(head.next, val);
+//         }
+
+//         if(head.val == val){ 
+            
+//            return removeElements(head.next, val);
+//         }
+
+//         return head;
+//     }
+// }
